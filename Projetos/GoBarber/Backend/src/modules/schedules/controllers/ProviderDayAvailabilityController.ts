dfb57ed87c2e540b36269ed4a95f6156d7f02327
Guilherme Padilha <gs.padilha@gmail.com>
2020-10-27 10:36:58 -0300

    fix: get deve receber apenas querys

--- Projetos/GoBarber/Backend/src/modules/schedules/controllers/ProviderDayAvailabilityController.ts
+++ Projetos/GoBarber/Backend/src/modules/schedules/controllers/ProviderDayAvailabilityController.ts
@@ -6,7 +6,7 @@ import ListProviderDayAvailabilityService from '@modules/schedules/services/List
 export default class ProviderDayAvailabilityController {
   public async index(request: Request, response: Response): Promise<Response> {
     const { provider_id } = request.params;
-    const { day, month, year } = request.body;
+    const { day, month, year } = request.query;
 
     const service = container.resolve(ListProviderDayAvailabilityService);
 
